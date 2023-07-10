2202d338c89109180819ff2893ce83deffce5898
Rajeev Kumar <rajeevk9@birlasoft.com>
2023-07-10 15:30:42 +0530

    erporting

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once('/usr/local/lib/phpqrcode/qrlib.php');
 // Test QR code generation
-QRcode::png('Hello, World!');
+QRcode::png('Hello, World!');die();
 
 
 
