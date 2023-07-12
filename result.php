0092a90fd34de9de03dc9d09c69a1031243183d9
Rajeev Kumar <rajeevk9@birlasoft.com>
2023-07-12 18:03:06 +0530

    file

--- result.php
+++ result.php
@@ -112,8 +112,6 @@
 	  margin-top: 20px;
 	  background: #1DA1F2;
 	  height: 50px;
-	  width:20em;
-	  font-size: 18px
 	  border-radius: 9px;
 	  cursor: pointer;
 	  font-weight: 900;
@@ -190,7 +188,7 @@
 						<textarea class="form-control" name="prbdetails" style="width:20em;" value="<?php echo @$prbdetails; ?>" required placeholder="Enter Your Problems in Details"></textarea>
 					</div>
 					<div class="form-group">
-						<input type="submit" name="submit" class="btn btn-primary submitBtn" />
+						<input type="submit" name="submit" class="btn btn-primary submitBtn" style="font-size: 18px;width: 278px;"/>
 					</div>
 					</div>
 				</div>
