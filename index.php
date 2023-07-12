febad15024206432858b41ee643da29eea0ca1ae
Rajeev Kumar <rajeevk9@birlasoft.com>
2023-07-12 08:25:16 +0530

    QR update

--- index.php
+++ index.php
@@ -101,15 +101,15 @@ if(isset($_POST['submit']) ) {
 				$filename = "author";
 			}
 			?>
-			<div class="q">
+			<!--div class="q">
 				<h2 style="text-align:center">QR Code Result: </h2>
 				<center>
 					<div class="qrframe" style="border:2px solid black; width:210px; height:210px;">
-							<?php echo '<img src="temp/'. @$filename.'.png" style="width:200px; height:200px;"><br>'; ?>
+							<?php //echo '<img src="temp/'. @$filename.'.png" style="width:200px; height:200px;"><br>'; ?>
 					</div>
-					<a class="btn btn-primary submitBtn" style="width:210px; margin:5px 0;" href="download.php?file=<?php echo $filename; ?>.png ">Download QR Code</a>
+					<a class="btn btn-primary submitBtn" style="width:210px; margin:5px 0;" href="download.php?file=<?php //echo $filename; ?>.png ">Download QR Code</a>
 				</center>
-			</div>
+			</div--->
 			
 		</div>
 </body>
