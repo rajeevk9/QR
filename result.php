e9e9f68101d68825311db84351c96eaee88fc7ba
Rajeev Kumar <rajeevk9@birlasoft.com>
2023-07-16 13:34:34 +0530

    updates

--- result.php
+++ result.php
@@ -91,7 +91,7 @@
 	</head>
 	<body onload="startTime()" style="font-size: 1.5rem !important;">
 		<div class="container" style="padding: 24px;">
-				<form method="post" action="thankyou.php?caseid=".<?php echo $productNumber;?> >
+				<form method="post" action='"thankyou.php?caseid='.<?php echo @$productNumber; ?>.'"' >
 				<div class="row">
 				<div class="prefilled-prd-details" style="width: 100%; margin-top:-14px;">
 				<img src="img/hlogo.png" style="width: 50%;margin-left: 25%;">
